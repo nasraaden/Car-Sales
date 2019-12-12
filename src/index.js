215b59371bb291ac8dd7548356170ca422521f0e
Nasra <nasraaden@Nasras-MBP.wifi.pcc.edu>
2019-12-12 09:10:27 -0800

    finished mvp

--- src/index.js
+++ src/index.js
@@ -1,9 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
+
+
 import App from './App';
+
+import { carReducer } from './store/reducers/index'
 
 import 'bulma/css/bulma.css';
 import './styles.scss';
 
-const rootElement = document.getElementById('root');
-ReactDOM.render(<App />, rootElement);
+const store = createStore(carReducer);
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  rootElement
+);
+
